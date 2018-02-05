248baf61ff905e51717b0abffbe5c511ed29f1fc
Marie Hoffmann <marie.hoffmann@fu-berlin.de>
2018-02-05 14:14:06 +0100

    Rename one2expand.py to dna4_convert.py

--- apps/dna4_convert.py
+++ apps/dna4_convert.py
@@ -2,9 +2,12 @@
     ============================================================================
                 GreenBox - Script Collection for Metagenomics
     ============================================================================
-    Convert 1 letter coded DNA sequence into a set of sequences permitting only
-    standard letters 'A', 'C', 'G', 'T' in FASTA format. "name" plus counter 
-    will be used as an accession number.
+    Translate DNA sequence to reduced DNA4 alphabet ('A', 'C', 'G', 'T') in FASTA 
+    format by expanding one sequence to all possible sequences. E.g. a sequence 
+    containing two extra Y's at position i and j, will be translated into 2**2=4 
+    sequences with 'C' or 'T' at the
+    The input argument <name> concatenated with a counter will be used as an 
+    accession number. The output is an expanded
     
     ============================================================================
     Author: Marie Hoffmann <marie.hoffmann AT fu-berlin.de>
@@ -45,16 +48,12 @@ def expand(seq):
     l = [seq]
     while is_converted(l[0]) is False:
         idx = next_idx(l[0])
-        print idx
         l_aux = []
         for s in l:
             for letter in d[l[0][idx]]:
                 s_new = copy.copy(s[:idx] + letter + s[idx+1:])
                 l_aux.append(s_new)
         l = l_aux
-    print l[:10]
-    print(len(l))
-    print(len(set(l)))
     return l
 
 def writeFASTA(seqs, headers, filename):
@@ -64,7 +63,7 @@ def writeFASTA(seqs, headers, filename):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage python one2four <name> <sequence> <outputfile>")
+        print("Usage python dna4convert.py <name> <sequence> <outputfile>")
     else:
         seqs = expand(sys.argv[2])
         writeFASTA(seqs, [sys.argv[1] + str(i) for i in range(1, len(seqs)+1)], sys.argv[3])
